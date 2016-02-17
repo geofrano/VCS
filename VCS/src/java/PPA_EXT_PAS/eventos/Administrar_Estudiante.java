bc4445068e12f73794372b05cbd154c8ec0964f3
geofrano <geofrano@hotmail.com>
2016-02-17 00:01:39 -0500

    Cambios luisin

--- VCS/src/java/PPA_EXT_PAS/eventos/Administrar_Estudiante.java
+++ VCS/src/java/PPA_EXT_PAS/eventos/Administrar_Estudiante.java
@@ -5,15 +5,25 @@
  */
 package PPA_EXT_PAS.eventos;
 
+import PPA_EXT_PAS.dominio.Carta_Compromiso;
 import PPA_EXT_PAS.dominio.Estudiante;
+import PPA_EXT_PAS.dominio.Menu_principal;
 import accesodatos.AccesoDatos;
 import accesodatos.ConjuntoResultado;
 import accesodatos.Parametro;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  *
@@ -24,54 +34,74 @@ public class Administrar_Estudiante {
     public Administrar_Estudiante(){
         super();
     }
-    public void consulta_carta_compro(HttpServletResponse response, HttpServletRequest request) throws IOException{
-        PrintWriter out = response.getWriter();
+    public static List<Carta_Compromiso> consulta_carta_compro(String nombre_estudiante) throws IOException{
+        List<Carta_Compromiso> datos = new LinkedList<Carta_Compromiso>();
+
         ArrayList<Parametro> parametro = new ArrayList<>();
-        String sql = "SELECT id_carta_comp, \n" +
-                     "       secuencial, \n" +
-                     "       \"Nomb_estudiante\",\n" +
-                     "       nomb_empresa, \n" +
-                     "       \"Lugar_suscripcion\",\n" +
-                     "       \"Fecha_suscripcion\",\n" +
-                     "       \"Estado\"\n" +
-                     "  FROM \"VCS_CARTA_COMPROMISO\"\n" +
-                     " WHERE UPPER(\"Nomb_estudiante\") LIKE UPPER('%' || ? || '%')";
-        String nombre_estudiante=(String)request.getParameter("id_est");
+        String sql = "SELECT cc_id, \n" +
+                     "       es_nombre,\n" +
+                     "       es_apellido,\n" +
+                     "       cc_lugar_suscripcion, \n" +
+                     "       to_char(cc_fecha_suscripcion,'dd/mm/yyyy'), \n" +
+                     "       cc_estado\n" +
+                     "  FROM \"MPP_CARTA_COMPROMISO\" CC,\n" +
+                     "       \"MPP_ESTUDIANTES\" ES\n" +
+                     " WHERE CC.es_id = ES.es_id\n" +
+                     "   AND (UPPER(es_nombre) LIKE UPPER('%' || ? || '%') \n" +
+                     "	OR UPPER(es_apellido) LIKE UPPER('%' || ? || '%'))";
+        
+        
         parametro.add(new Parametro(1, nombre_estudiante));
+        parametro.add(new Parametro(2, nombre_estudiante));
+        Carta_Compromiso carta_comp;
         try {
             ConjuntoResultado cres = AccesoDatos.ejecutaQuery(sql, parametro);
-            out.println("<table class=\"table table-hover table-responsive\">\n");
-            out.println("<tr>\n");
-            out.println("<td>Id Carta Compromiso</td>\n");
-            out.println("<td>No</td>\n");
-            out.println("<td>Nombre Estudiante</td>\n");
-            out.println("<td>Nombre Empresa</td>\n");
-            out.println("<td>Lugar SuscripciÃ³n</td>\n");
-            out.println("<td>Fecha SuscripciÃ³n</td>\n");
-            out.println("<td>Estado del Tramite</td>\n");
-            out.println("<td></td><td></td></tr>\n");
+  
             while (cres.next()) {
-                out.println("<tr>\n");
-                out.println("<td>" + cres.getString(0) + "</td>\n");
-                out.println("<td class=\"alineado3\">" + cres.getInt(1) + "</td>\n");
-                out.println("<td>" + cres.getString(2) + "</td>\n");
-                out.println("<td>" + cres.getString(3) + "</td>\n");
-                out.println("<td>" + cres.getString(4) + "</td>\n");
-                out.println("<td class=\"alineado3\">" + cres.getDate(5) + "</td>\n");
-                out.println("<td class=\"alineado3\">" + cres.getString(6) + "</td>\n");
-                out.println("<td class=\"alineado3\">"
-                            + "<img width=\"20px\" height=\"20px\" src=\"../../images/icono_modifica.png\"/>"
-                            + "</td>\n");
-                out.println("<td class=\"alineado3\">"
-                            + "<img width=\"20px\" height=\"20px\" src=\"../../images/eliminar.jpg\"/>"
-                            + "</td>\n");
-                out.println("</tr>");
+                carta_comp = new Carta_Compromiso();
+                carta_comp.setId_carta_compromiso(cres.getString(0).trim());
+                carta_comp.setNomb_estudiante(cres.getString(1).trim());
+                carta_comp.setNomb_empresa(cres.getString(2).trim());
+                carta_comp.setLugar_suscripcion(cres.getString(3).trim());
+                carta_comp.setFecha_suscripcion(cres.getString(4).trim());
+                carta_comp.setEstado(cres.getString(5).trim());
+                System.out.println("EST: "+cres.getString(1).trim());
+                datos.add(carta_comp);
             }
-            out.println("</table>");
+            //out.println("</table>");
         } catch (Exception e) {
-            out.close();
             throw new RuntimeException(e);
         }
-        
+        return datos;
+    }
+    public static JSONObject toJSONObject(Carta_Compromiso carta_comp) {
+        JSONObject json = new JSONObject();
+        try {
+            json.put("id_cc", carta_comp.getId_carta_compromiso());
+            json.put("nomb_est", carta_comp.getNomb_estudiante());
+            json.put("emp_nombre", carta_comp.getNomb_empresa());
+            json.put("lugar_suscrip", carta_comp.getLugar_suscripcion());
+            json.put("fecha_suscrip", carta_comp.getFecha_suscripcion());
+            //json.put("estado", carta_comp.getEstado());
+            
+        } catch (JSONException ex) {
+            Logger.getLogger(Carta_Compromiso.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return json;
+    }
+    public static JSONObject toJSON(List< Carta_Compromiso >  dato) {
+        JSONObject json = new JSONObject();
+        try {
+            JSONArray jsonItems = new JSONArray();
+            for (Iterator<Carta_Compromiso> it = dato.iterator(); it.hasNext();) {
+                Carta_Compromiso campo = it.next();
+                jsonItems.put( toJSONObject( campo ) );
+            }
+
+            json.put("items", jsonItems);
+        } catch (JSONException ex) {
+            Logger.getLogger(Menu_principal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return json;
     }
 }
